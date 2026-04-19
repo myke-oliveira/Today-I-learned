8f0bc2ac9642032eef92f12822c5938645c0efd7
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2026-04-19 12:00:39 -0300

    Refactor output examples to use Console for rich printing

--- richer_examples.py
+++ richer_examples.py
@@ -1,4 +1,14 @@
-from rich import print
+from rich.console import Console
+from rich.theme import Theme
 
-print(f"I wonder what this looks like 1 + 1 = {1 + 1}")
-print({"a": [1, 2, 3], "b" : {"c" : 1}})
+
+console = Console()
+
+console.print(f"I wonder what this looks like 1 + 1 = {1 + 1}")
+console.print({"a": [1, 2, 3], "b" : {"c" : 1}})
+
+console.print("This is [bold magenta]rich[/bold magenta]!", ":thumbs_up:")
+console.print("This is some text!", ":thumbs_up:", style="bold underline")
+console.print("This is some text!", ":thumbs_up:", style="bold underline red")
+console.print("This is some text!", ":thumbs_up:", style="bold underline red on white")
+console.print("[bold]This [underline]is[/] some text![/]")
